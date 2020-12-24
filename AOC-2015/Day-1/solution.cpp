74f87c56954d75c618fa3bf6ba741be4af04b4a6
SubHodH-K-N <subhodhkoodavalli@gmail.com>
2020-12-24 20:59:14 +0530

    Modified the solution.cpp

--- AOC-2015/Day-1/solution.cpp
+++ AOC-2015/Day-1/solution.cpp
@@ -1,26 +1,58 @@
-#include <iostream>
-#include <fstream>
-
+#include <bits/stdc++.h>
 using namespace std;
+
+class Day1 {
+	private:
+		string floors;
+		int count;
+
+	public:
+		Day1(string x) {
+			floors = x;
+		}
+
+		void partOne() {
+			count = 0;
+			for(int i = 0; i < floors.length(); i++) {
+				if(floors[i] == '(') {
+					count++;
+				} else {
+					count--;
+				}
+			}
+
+			cout << count << '\n';	
+		}
+
+		void partTwo() {
+			count = 0;
+			for(int i = 0; i < floors.length(); i++) {
+				if(floors[i] == '(') {
+					count++;
+				} else {
+					count--;
+				}
+
+				if(count == -1) {
+					cout << i + 1 << '\n';
+					break;
+				}
+			}
+		}
+};
 
 int main()
 {
 	string x;
-	int count = 0;
 	fstream fcin;
 	fcin.open("input", ios::in);
 	fcin >> x;
-	for (int i = 0; i < x.length(); i++) {
-		if(x[i] == '(')
-			count++;
-		else 
-			count--;
 
-		//PART 2 Solution
-		/*if(count == -1) {
-			cout << i + 1 << "\n";
-			break;
-		}*/
-	}
-	cout << count << "\n";
+	Day1 one(x);
+	
+	//Output the solution
+	cout << "2015 Day 1 Part 1 Solution:" << '\n';
+	one.partOne();
+	cout << "2015 Day 1 Part 2 Solution:" << '\n';
+	one.partTwo();
 }
